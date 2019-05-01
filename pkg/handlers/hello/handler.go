893aedb34180a54cb31a57f26e3e0bc750928b27
Silvio Greuel <silviogreuel@gmail.com>
2019-04-30 21:18:46 -0300

    Adding usecase

--- pkg/handlers/hello/handler.go
+++ pkg/handlers/hello/handler.go
@@ -3,12 +3,17 @@ package hello
 import (
 	"net/http"
 
+	domain "github.com/eventials/vlab-boilerplate-api/pkg/hello"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Attach(g *gin.RouterGroup) {
 	g.GET("/:name", func(c *gin.Context) {
 		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s", name)
+
+		result := domain.SayHello(name)
+
+		c.String(http.StatusOK, result)
 	})
 }
